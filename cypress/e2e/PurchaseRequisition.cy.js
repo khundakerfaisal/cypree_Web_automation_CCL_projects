e6ba01c103a9164b9d439962b03fa819a3499a4f
khundaker Faisal <khundakermis@gmail.com>
2024-04-23 19:16:15 +0600

    PR selection part start

--- cypress/e2e/PurchaseRequisition.cy.js
+++ cypress/e2e/PurchaseRequisition.cy.js
@@ -8,6 +8,12 @@ describe("Create Purchase Request", () => {
     it("Create PR", () => {
         cy.get('.dropdown-toggle > .oi').click()
         cy.get('[href="#menu_id=309&action=502"]').click()
+        cy.get('.d-xl-none > .o_list_button_add').click()
+        cy.get('#priority_0').select('High')
+        cy.get('#requisition_type_0').select('Local Purchase')
+        cy.get('#budget_id_0').type('DevOps Budget - Entertainment - 2023-2024 (267169.75) (905648.22)').click()
+
+        // cy.xpath("").select('High')  
         // cy.xpath("//a[contains(.,'Purchase')]").click()  
 
     })
